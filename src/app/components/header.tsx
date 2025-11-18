c462ee4b476ba496748afee6efdeed15c134bbb8
Matheus <matheus_lopes151@hotmail.com>
2025-11-17 22:14:58 -0300

    atualização das tecnologias

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,15 +1,30 @@
+"use client";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import SplitText from "@/components/SplitText";
+
 
 export function Header() {
+
     return (
-        <header className="w-full text-white p-4">
+        <header id="header" className="w-full text-white p-4">
             <div className="flex flex-col md:flex-row md:justify-between items-center">
-                <div>
-                    <h1 className="text-2xl font-bold">Matheus <span className="text-white hover:text-slate-400 duration-300">Dev</span></h1>
-                </div>
+                <SplitText
+                    text="Matheus Dev !"
+                    className="text-2xl font-bold text-center text-white"
+                    delay={500}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+
+                />
                 <div className="flex mt-4 md:mt-0">
                     <FaGithub size={24} className="text-white hover:text-slate-400 duration-300 cursor-pointer mr-4" />
                     <FaLinkedin size={24} className="text-white hover:text-slate-400 duration-300 cursor-pointer" />
--- src/app/components/tecnlogies.tsx
+++ src/app/components/tecnlogies.tsx
@@ -1,37 +1,166 @@
+"use client";
 import { FaHtml5, FaCss3Alt, FaNodeJs, FaReact, FaSass } from "react-icons/fa";
 import { RiTailwindCssFill, RiNextjsFill } from "react-icons/ri";
-import { SiTypescript } from "react-icons/si";
+import { SiTypescript, SiPrisma, SiGit, SiGithub, SiStripe } from "react-icons/si";
+import GlareHover from "@/components/GlareHover";
+
+
+
 export function Tecnologies() {
     return (
         <div className="mt-10 md:mt-20 ">
+
+
+
             <h2 className="text-3xl md:text-4xl text-white text-center font-bold mb-8">Tecnologias que utilizo</h2>
+
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10 px-4 md:px-0 mx-auto mb-12">
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <FaHtml5 className="w-12 h-12 text-red-600 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center ">
+
+                </GlareHover>
+
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <FaCss3Alt className="w-12 h-12 text-blue-600 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+
+
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <FaNodeJs className="w-12 h-12 text-amber-300 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <FaReact className="w-12 h-12 text-blue-400 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <FaSass className="w-12 h-12 text-rose-400 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <RiTailwindCssFill className="w-12 h-12 text-blue-400 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <RiNextjsFill className="w-12 h-12 text-slate-800 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
-                <div className="bg-white p-2 rounded-lg flex items-center justify-center">
+                </GlareHover>
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
                     <SiTypescript className="w-12 h-12 text-blue-600 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
-                </div>
+                </GlareHover>
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                    <SiPrisma className="w-12 h-12 text-white animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
+                </GlareHover>
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                    <SiGit className="w-12 h-12 text-orange-600 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
+                </GlareHover>
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                    <SiGithub className="w-12 h-12 text-white animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
+                </GlareHover>
+
+                <GlareHover
+                    className=" p-2"
+                    glareColor="#ffffff"
+                    glareOpacity={0.3}
+                    glareAngle={-30}
+                    glareSize={200}
+                    transitionDuration={800}
+                    playOnce={false}
+                >
+                    <SiStripe className="w-12 h-12 text-violet-500 animate-rotate-x animate-infinite animate-duration-[2000ms] animate-ease-linear" />
+                </GlareHover>
 
             </div>
-        </div>
+        </div >
     )
 }
