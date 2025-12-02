56154ff336edea39e517173c56c525c141948ffa
Matheus <matheus_lopes151@hotmail.com>
2025-12-01 23:07:48 -0300

    cards

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -10,6 +10,25 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 import TrueFocus from "@/components/TrueFocus";
+import foto from "@/assets/landing.jpg"
+
+interface CarouselItem {
+    id: number;
+    title: string;
+    image?: string | { src: string };
+    url?: string;
+}
+
+const data: CarouselItem[] = [
+    { id: 1, title: "landing page xyz", image: "https://unsplash.com/pt-br/fotografias/familia-colhendo-macas-de-uma-arvore-em-um-pomar-0VIqR7HmDZw", url: "https://github.com/matheusld34/landing-page-com-grid-agencia-xyz" },
+    { id: 2, title: "Item 2" },
+    { id: 3, title: "Item 3" },
+    { id: 4, title: "Item 4" },
+    { id: 5, title: "Item 5" },
+    { id: 6, title: "Item 6" },
+    { id: 7, title: "Item 7" },
+    { id: 8, title: "Item 8" },
+]
 
 export function CarouselSize() {
     return (
@@ -38,12 +57,13 @@ export function CarouselSize() {
                     className="w-full max-w-7xl max-h-fit container mx-auto"
                 >
                     <CarouselContent>
-                        {Array.from({ length: 10 }).map((_, index) => (
-                            <CarouselItem key={index} className="sm:pl-2 md:basis-1/3 lg:basis-1/3">
+                        {data.map((item) => (
+                            <CarouselItem key={item.id} className="sm:pl-2 md:basis-1/3 lg:basis-1/3">
                                 <div className="p-1">
                                     <Card>
                                         <CardContent className="flex aspect-square items-center justify-center p-6">
-                                            <span className="text-3xl font-semibold">{index + 1}</span>
+                                            <img src={typeof item.image === 'string' ? item.image : item.image?.src} alt={item.title} className="rounded-md mb-4" />
+                                            <span className="text-3xl font-semibold">{item.title}</span>
                                         </CardContent>
                                     </Card>
                                 </div>
